a8bb3a0c088e1821256ee2d0a28531162667606d
hosunjoo <165250860+hosunjoo@users.noreply.github.com>
2024-10-31 15:33:51 +0900

    Update sidemenu.js

--- sidemenu.js
+++ sidemenu.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll('.sidebar .nav-links li a');
 
+    // 현재 페이지의 파일 이름 가져오기
+    const currentPage = window.location.pathname.split("/").pop();
+
     navLinks.forEach(link => {
+        // href 속성이 현재 페이지와 같으면 active 클래스 추가
+        if (link.getAttribute('href') === currentPage) {
+            link.classList.add('active');
+        }
+
+        // 메뉴 항목 클릭 시 다른 항목의 active 클래스 제거
         link.addEventListener('click', function() {
-            // 모든 링크에서 active 클래스를 제거
             navLinks.forEach(item => item.classList.remove('active'));
-            // 클릭한 링크에만 active 클래스를 추가
             this.classList.add('active');
         });
     });
