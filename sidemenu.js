6be0f0d7b0a56a76d87deb10fb486c1df203e092
hosunjoo <165250860+hosunjoo@users.noreply.github.com>
2024-10-31 13:53:17 +0900

    Update sidemenu.js

--- sidemenu.js
+++ sidemenu.js
@@ -1,6 +1,6 @@
 $(function() {
-  $(document).on('click', 'ul>li', function() {
-    $(this).closest('ul').find('li.active').removeClass('active');
-    $(this).addClass('active');
+  $(document).on('click', 'li', function() {
+    $('li.active').removeClass('active'); // 모든 li 요소의 active 클래스를 제거
+    $(this).addClass('active'); // 클릭한 li 요소에 active 클래스를 추가
   });
 });
